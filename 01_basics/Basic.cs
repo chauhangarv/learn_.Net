b5c393f0a60a67abbc1318e72207cc73a4e4c2b3
Garv Chauhan <chauhangarv37@gmail.com>
2024-03-08 13:53:47 +0530

    first commit

---
+++ 01_basics/Basic.cs
@@ -0,0 +1,62 @@
+/*
+C# programming language games, windows GUI, Console application bnane m bhut use aati h...C# dot net 
+framework m run krti h, isiliye ye dot net ka part h
+*/
+
+/*
+.Net framework ek open source developer platform h, iska mtlb isme community contribution aa skte h iska source code github pr rkha hua h hum isme commit kr skte h....ye ek cross-platform developer platform h it means ye sbhi OS m chl skta h
+*/
+
+// .Net Framework --> it supports only in windows
+// .Net Core --> it is cross-platform implementation
+
+// write & WriteLine m ek diff h ki ye line break nhi krta h
+
+// namespace --> ye ek container hota h jisme humari classes and other namespaces hote h
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// namespace Hello
+// {
+//     class Basic
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World");
+//             Console.Write("Hello India");
+//             Console.WriteLine("Harry Potter");
+//             // Console.ReadLine();
+//         }
+//     }
+// }
+
+
+namespace Hello
+{
+    class Basic
+    {
+        public static void Main(string[] args)
+        {
+            int a = 35;
+            int b = 40;
+            Console.WriteLine(a + b);
+        }
+    }
+}
+
+
+/*
+Data Types:-
+Integer - int hello;
+Floating point number - float that;
+Character - char a = 'A';
+Boolean - bool isGreat = true;
+String - string inp = "Hello";
+*/
---
+++ 02_basics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    
+    namespace Client
+    {
+        public class Program
+        {
+            static void Great(string name)
+            {
+                Console.WriteLine("HARRY POTTER"+ name);
+            }
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Hello");
+        Great(" Wizard");
+        Great(" Magician");
+            }
+        }
+    }

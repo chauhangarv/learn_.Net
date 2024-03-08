063b78d13a46defbfea854ef424b53e0d58e7e99
Garv Chauhan <chauhangarv37@gmail.com>
2024-03-08 14:04:02 +0530

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+            public class Program
+        {
+            public static void Main(string[] args)
+            {
+               int num = 9;
+               int fact = 1;
+
+               for (int i = 1; i <= num; i++)
+               {
+                fact = fact * i;
+               }
+               Console.WriteLine(fact);
+        }
+    }
+}
